97f19c4db21c585e6f1280e4f81d6be88131bb60
Manh Duy <Duymmpd95@gmail.com>
2016-11-11 10:36:10 +0700

    update_praticel2_MaiManhDuy_2016/11/11_intent_phone_call

---
+++ app/src/main/java/vn/asiantech/training/CallPhone.java
@@ -0,0 +1,13 @@
+package vn.asiantech.training;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CallPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_phone);
+    }
+}
