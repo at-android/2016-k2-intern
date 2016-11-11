84ed74666b698b3a2cafac90d789b4717c8673db
Manh Duy <Duymmpd95@gmail.com>
2016-11-11 15:45:35 +0700

    update_praticel2_MaiManhDuy_2016/11/11_intent_phone_call_1

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -2,11 +2,7 @@ package vn.asiantech.training;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -14,27 +10,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton btList = (ImageButton) findViewById(R.id.btListNumber);
-        btList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myInten = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01239543423"));
-                startActivity(myInten);
-            }
-        });
+/*        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (Intent.ACTION_DIAL.equals(action)) {
+
+        }*/
         waitData();
     }
 
     public void waitData() {
         Intent intent = getIntent();
-        if (intent.getData() != null && Intent.ACTION_DIAL.equals(intent.getAction())) {
-            Log.d("intent received", intent.getData().toString());
-            String phoneNumber = intent.getData().toString();
-            phoneNumber = phoneNumber.substring(4);
-            Log.d("intent received", "Received phone number : " + phoneNumber);
-        } else {
-            Log.d("intent received", "null intent received");
-        }
+        String action = intent.getAction();
+
     }
 
 }
