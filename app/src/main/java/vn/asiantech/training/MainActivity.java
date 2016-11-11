720083f2c228bb99cb51517843ce4af852fc90e8
Phuongpham09 <phuong.pham@nguonviet.vn>
2016-11-11 08:56:27 +0700

    Thuc hien chuc nang ve camera, galery,web,gg play,mail, message

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -3,12 +3,20 @@ package vn.asiantech.training;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTvCall;
@@ -19,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTvMap;
     private TextView mTvCamera;
     private TextView mTvGalery;
+    private ImageView mImvPicture;
+    private int REQUEST_CAMERA = 100;
+    private int REQUEST_GALERY = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +44,11 @@ public class MainActivity extends AppCompatActivity {
         mTvMap = (TextView) findViewById(R.id.tvMap);
         mTvCamera = (TextView) findViewById(R.id.tvCamera);
         mTvGalery = (TextView) findViewById(R.id.tvGalery);
+        mImvPicture = (ImageView) findViewById(R.id.imvPicture);
 
         mTvCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //goi dien
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:01645484112"));
                 if (ActivityCompat.checkSelfPermission(MainActivity.this,
@@ -51,43 +62,107 @@ public class MainActivity extends AppCompatActivity {
         mTvMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //goi tin nhan
+                sendSMS("01645484112", "test Send Message");
             }
         });
 
         mTvMail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //goi mail
+                sendMail();
             }
         });
 
         mTvWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mo web
+                lauchWeb();
             }
         });
 
         mTvGalery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mo thu vien anh
+                lauchGalery();
             }
         });
 
         mTvCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mo camera
+                lauchCamera();
             }
         });
 
         mTvStore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mo web
+                lauchPlayStore();
             }
         });
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, null, null);
+    }
+
+    private void sendMail() {
+        String subject = "Demo subject";
+        String message = "Demo message";
+        String to = "tamnm@asiantech.com";
+
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+        email.putExtra(Intent.EXTRA_SUBJECT, subject);
+        email.putExtra(Intent.EXTRA_TEXT, message);
+
+        //need this to prompts email client only
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
+    }
+
+    private void lauchPlayStore() {
+        final Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://play.google.com/store/apps/"));
+        startActivity(intent);
+
+    }
+
+    private void lauchWeb() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.vnexpress.net"));
+        startActivity(intent);
+    }
+
+    private void lauchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CAMERA);
+        }
+    }
+
+    private void lauchGalery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_GALERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImvPicture.setImageBitmap(imageBitmap);
+        }
+        if (requestCode == REQUEST_GALERY) {
+            Uri selectedImage = data.getData();
+            InputStream imageStream = null;
+            try {
+                imageStream = getContentResolver().openInputStream(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+            mImvPicture.setImageBitmap(bitmap);
+        }
     }
 }
