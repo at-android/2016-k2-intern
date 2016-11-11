47dd5912603c90d04db0bd6f41dc357f138068aa
duyle112 <duyle.bks2014@gmail.com>
2016-11-11 09:44:57 +0000

    Finish Ex1 and Create button dial Ex2 Commit

--- app/src/main/java/vn/asiantech/training/DemoIntentFilter.java
+++ app/src/main/java/vn/asiantech/training/DemoIntentFilter.java
@@ -2,18 +2,21 @@ package vn.asiantech.training;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.List;
 
 public class DemoIntentFilter extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,6 +31,7 @@ public class DemoIntentFilter extends AppCompatActivity implements View.OnClickL
     private Button mBtnsendEmail;
     private Button mBtncamera;
     private ImageView mImgdisplay;
+    private Button mBtncall2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,7 @@ public class DemoIntentFilter extends AppCompatActivity implements View.OnClickL
         mBtnsendEmail = (Button) findViewById(R.id.btnSendemail);
         mBtncamera = (Button) findViewById(R.id.btnCamera);
         mImgdisplay = (ImageView) findViewById(R.id.Imgdisplay);
+        mBtncall2 = (Button) findViewById(R.id.btnDial);
         mBtncall.setOnClickListener(this);
         mBtnlaunchWeb.setOnClickListener(this);
         mBtnmessage.setOnClickListener(this);
@@ -54,40 +59,41 @@ public class DemoIntentFilter extends AppCompatActivity implements View.OnClickL
         mBtnopenGallery.setOnClickListener(this);
         mBtnsendEmail.setOnClickListener(this);
         mBtncamera.setOnClickListener(this);
+        mBtncall2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.Btncall:
+            case R.id.btnCall:
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:0377778888"));
                 if (callIntent.resolveActivity(getPackageManager()) != null) {
                     startActivity(callIntent);
                 }
                 break;
-            case R.id.Btncamera:
+            case R.id.btnCamera:
                 Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intentCamera, REQUEST_IMAGE_CAPTURE);
                 break;
-            case R.id.BtnlaunchWeb:
+            case R.id.btnLaunchweb:
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW);
                 browserIntent.setData(Uri.parse("http://www.vnexpress.net"));
                 if (browserIntent.resolveActivity(getPackageManager()) != null) {
                     startActivity(browserIntent);
                 }
                 break;
-            case R.id.Btnmessage:
+            case R.id.btnMessage:
                 Intent sendIntent = new Intent(Intent.ACTION_VIEW);
                 sendIntent.setData(Uri.parse("sms:"));
                 startActivity(sendIntent);
                 break;
-            case R.id.BtnopenGallery:
+            case R.id.btnOpengallery:
                 Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
                 photoPickerIntent.setType("image/*");
                 startActivityForResult(photoPickerIntent, REQUEST_IMAGE_PICK);
                 break;
-            case R.id.BtnopenGGmap:
+            case R.id.btnOpenggmap:
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
                 String data = String.format("geo:%s,%s", 100, 200);
@@ -96,18 +102,28 @@ public class DemoIntentFilter extends AppCompatActivity implements View.OnClickL
                     startActivity(intent);
                 }
                 break;
-            case R.id.BtnopenGGstore:
+            case R.id.btnOpenggstore:
                 Intent intentGGstore = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("market://details?id=" + getApplicationContext().getPackageName()));
                 if (intentGGstore.resolveActivity(getPackageManager()) != null) {
                     startActivity(intentGGstore);
                 }
                 break;
-            case R.id.BtnsendEmail:
+            case R.id.btnSendemail:
                 Intent intentSend = new Intent(Intent.ACTION_SEND);
                 intentSend.setType("plain/text");
                 if (intentSend.resolveActivity(getPackageManager()) != null) {
                     startActivity(Intent.createChooser(intentSend, ""));
+                }
+                break;
+            case R.id.btnDial:
+                Intent mainIntent = new Intent(Intent.ACTION_DIAL, null);
+                mainIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                List<ResolveInfo> pkgAppsList = getApplicationContext().getPackageManager().queryIntentActivities(mainIntent, 0);
+                if (pkgAppsList.size() > 0) {
+                    Intent chooser = Intent.createChooser(mainIntent, "Choose");
+                    Log.i("info", chooser.toString());
+                    startActivity(chooser);
                 }
                 break;
         }
