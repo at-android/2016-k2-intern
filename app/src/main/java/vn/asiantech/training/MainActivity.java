7a83a39edc2f04f112845856bcdd02dac79ee2e7
Manh Duy <Duymmpd95@gmail.com>
2016-11-11 09:22:55 +0700

    new_praticel2_MaiManhDuy_2016/11/10_intent_filter_phone_call

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -1,13 +1,24 @@
 package vn.asiantech.training;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        if (intent.getData() != null) {
+            Log.d("intent received", intent.getData().toString());
+            String phoneNumber = intent.getData().toString();
+            phoneNumber = phoneNumber.substring(4);
+            Log.d("intent received", "Received phone number : " + phoneNumber);
+        } else {
+            Log.d("intent received", "null intent received");
+        }
     }
 }
