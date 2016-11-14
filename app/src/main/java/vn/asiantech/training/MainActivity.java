e88a2ca60deb63f795139de9b30fd3b19f279f6f
sentinel18 <tanvo.bks2014@gmail.com>
2016-11-14 14:07:54 +0700

    first commit

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -1,13 +1,15 @@
 package vn.asiantech.training;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
