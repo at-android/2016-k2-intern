1de25a1700b5b22d02ff5305d7e950ae4a423488
Manh Duy <Duymmpd95@gmail.com>
2016-11-14 09:17:29 +0700

    update_praticel2_MaiManhDuy_2016/14/10_fix_id

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -1,14 +1,14 @@
 package vn.asiantech.training;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     ImageButton btCall;
     TextView txtNumber;
     String numberphone;
