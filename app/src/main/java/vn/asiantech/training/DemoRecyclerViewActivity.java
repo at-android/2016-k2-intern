35334ba105e1eead71be150116e099cc4fe1ae6b
duyle112 <duyle.bks2014@gmail.com>
2016-11-18 04:38:17 +0000

    New RecyclerView

---
+++ app/src/main/java/vn/asiantech/training/DemoRecyclerViewActivity.java
@@ -0,0 +1,13 @@
+package vn.asiantech.training;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DemoRecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_recycler_view);
+    }
+}
