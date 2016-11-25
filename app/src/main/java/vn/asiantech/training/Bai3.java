35b10ae7bfafbe8a9eb6e9f7dcea09fefbb09f1d
duyle112 <duyle.bks2014@gmail.com>
2016-11-25 04:36:01 +0000

    Bai1 Bai2

---
+++ app/src/main/java/vn/asiantech/training/Bai3.java
@@ -0,0 +1,14 @@
+package vn.asiantech.training;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Bai3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai3);
+
+    }
+}
