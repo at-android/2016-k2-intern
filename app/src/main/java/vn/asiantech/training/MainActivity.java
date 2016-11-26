fe13b287c5f262eefdfaf3857d2b807898e6fddd
Manh Duy <Duymmpd95@gmail.com>
2016-11-26 12:01:40 +0700

    praticle2_done

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -5,24 +5,28 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     Handler handler;
     Boolean check = true;
-    AtomicBoolean isrunning = new AtomicBoolean(false);
     private Button mBtnStart;
     private TextView mTvRandom;
     private TextView mTvRandomAfterSecond;
     private int FirsNumber = 0;
-    private int SecondNumber = 0;
     private int ResultNumber;
+    private TextView mTvTurn;
+    private TextView mTvTurnNumber;
+    private Timer mTimer;
+    private int mCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +35,17 @@ public class MainActivity extends AppCompatActivity {
         mBtnStart = (Button) findViewById(R.id.btnStart);
         mTvRandom = (TextView) findViewById(R.id.tvRandom);
         mTvRandomAfterSecond = (TextView) findViewById(R.id.tvRandomAfterSecond);
+        mTvTurn = (TextView) findViewById(R.id.tvTurnRandom);
+        mTvTurnNumber = (TextView) findViewById(R.id.tvTurnNumber);
         mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Random rad = new Random();
-                int number = rad.nextInt(99 - 10 + 1) + 10;
-                ResultNumber = number;
-                mTvRandom.setText(number + "");
+                ResultNumber = rad.nextInt(99 - 10 + 1) + 10;
+                mTvRandom.setText(ResultNumber + "");
                 doStart();
+                timecheck();
+                mBtnStart.setEnabled(false);
             }
         });
         handler = new Handler() {
@@ -47,10 +54,9 @@ public class MainActivity extends AppCompatActivity {
                 if (check) {
                     mTvRandomAfterSecond.setText(msg.arg1 + "");
                     FirsNumber = msg.arg1;
+                    check = false;
                 } else {
                     mTvRandomAfterSecond.setText(msg.arg1 + "" + FirsNumber);
-                    SecondNumber = msg.arg1;
-                    checkData();
                 }
                 super.handleMessage(msg);
             }
@@ -58,44 +64,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void doStart() {
-        isrunning.set(false);
+        check = true;
+        FirsNumber = 0;
         Thread th = new Thread(new Runnable() {
-
             @Override
             public void run() {
-                for (int i = 1; i <= 2 && isrunning.get(); i++) {
+                for (int i = 1; i <= 2; i++) {
                     SystemClock.sleep(1000);
+                    Message msg = handler.obtainMessage();
                     Random rad = new Random();
                     int number = rad.nextInt(9 + 1);
-                    if (i == 2) {
-                        check = false;
-                    }
-                    Message msg = handler.obtainMessage();
                     msg.arg1 = number;
                     handler.sendMessage(msg);
                 }
-
             }
 
         });
-        isrunning.set(true);
         th.start();
     }
 
-    void checkData() {
-        Thread ths = new Thread(new Runnable() {
+    public void timecheck() {
+        mTimer = new Timer();
+        mTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                SystemClock.sleep(1500);
-                if (SecondNumber == FirsNumber) {
-                    Log.d("a", "asd");
-                } else {
-                    Message msg = handler.obtainMessage();
-                    msg.arg1 = 10;
-                    handler.sendMessage(msg);
-                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = mTvRandomAfterSecond.getText().toString();
+                        int resulInt = Integer.parseInt(result);
+                        if (resulInt == ResultNumber) {
+                            mBtnStart.setEnabled(true);
+                            Toast.makeText(MainActivity.this, "Congratulations", Toast.LENGTH_SHORT).show();
+                            mTvTurn.setText(mCount + "");
+                            mTimer.cancel();
+                        } else {
+                            if (mCount < 14) {
+                                doStart();
+                                ++mCount;
+                                mTvRandomAfterSecond.setText("00");
+                            } else {
+                                ++mCount;
+                                mBtnStart.setEnabled(true);
+                                Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+                                mTimer.cancel();
+                            }
+                        }
+                        mTvTurnNumber.setText(mTvTurnNumber.getText() + " " + result);
+                        mTvTurn.setText(mCount + "");
+                    }
+                });
             }
-        });
-        ths.start();
+        }, 3500, 3500);
     }
 }
