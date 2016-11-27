b7b8575f3e3b5d58829cac1fc4aabb2a8847eddf
Manh Duy <Duymmpd95@gmail.com>
2016-11-27 09:48:11 +0700

    first_commit

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -1,13 +1,26 @@
 package vn.asiantech.training;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBtnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnStart = (Button) findViewById(R.id.btnStart);
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyAsynctask asynctask = new MyAsynctask(MainActivity.this);
+                asynctask.execute();
+                mBtnStart.setEnabled(false);
+            }
+        });
+
     }
 }
