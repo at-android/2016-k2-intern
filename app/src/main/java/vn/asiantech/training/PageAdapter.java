fd7088294e450f0bd7eafb418b818a0e5358ad65
duyle112 <duyle.bks2014@gmail.com>
2016-11-30 04:43:21 +0000

    tab1

---
+++ app/src/main/java/vn/asiantech/training/PageAdapter.java
@@ -0,0 +1,51 @@
+package vn.asiantech.training;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HoangDuy on 29/11/2016.
+ */
+public class PageAdapter extends FragmentStatePagerAdapter {
+    private final int PAGE_COUNT = 3;
+    private String mTabTitles[] = new String[]{"INTRO", "STATS", "EVO"};
+    private ArrayList<People> mPeoples;
+
+    public PageAdapter(FragmentManager fm, ArrayList<People> peoples) {
+        super(fm);
+        mPeoples = peoples;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                ContactFragment pageBOFragment = ContactFragment.newInstance(mPeoples);
+                return pageBOFragment;
+            case 1:
+                ContactFragment pageBOFragment1 = ContactFragment.newInstance(mPeoples);
+                return pageBOFragment1;
+
+            case 2:
+                ContactFragment pageBOFragment2 = ContactFragment.newInstance(mPeoples);
+                return pageBOFragment2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return mTabTitles[position];
+    }
+}
