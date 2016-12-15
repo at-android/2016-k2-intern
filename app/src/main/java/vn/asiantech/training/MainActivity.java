997b5aa4698f0bc9faff59013745bf4a610ceb0b
sentinel18 <tanvo.bks2014@gmail.com>
2016-12-15 14:53:58 +0700

    test

--- app/src/main/java/vn/asiantech/training/MainActivity.java
+++ app/src/main/java/vn/asiantech/training/MainActivity.java
@@ -1,10 +1,10 @@
 package vn.asiantech.training;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    int m;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
